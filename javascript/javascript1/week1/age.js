7690f5294fe9f1328b8846e5836775a12254df1d
besart-elezi <besart.elezi92@gmail.com>
2025-02-04 23:49:07 +0100

    added first task

---
+++ javascript/javascript1/week1/age.js
@@ -0,0 +1,6 @@
+/* Age-ify (A future age calculator) */
+
+let yearOfBirth = 1992;
+let yearFuture = 2032;
+let age = yearFuture - yearOfBirth;
+console.log("You will be " + age +" years old in " + yearFuture ) ;
