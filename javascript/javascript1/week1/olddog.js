9e453f96e4c821b0a2d74e298194c4a7a65b0c4b
besart-elezi <besart.elezi92@gmail.com>
2025-02-05 20:50:41 +0100

    added task2

---
+++ javascript/javascript1/week1/olddog.js
@@ -0,0 +1,9 @@
+let dogYearOfBirth = 2017;
+let dogYearFuture = 2027;
+let dogYear = dogYearFuture-dogYearOfBirth;
+let shouldShowResultInDogYears = false;
+if (shouldShowResultInDogYears){
+    console.log("Your dog will be " + dogYear * 7 +  " dog years old in " + dogYearFuture) ;
+} else{
+   console.log("Your dog will be "+ dogYear + " human years old in "  + dogYearFuture ) ;
+}
