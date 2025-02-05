48a7ff7c17c6fa66b39783166aa63bc77ddf34e4
besart-elezi <besart.elezi92@gmail.com>
2025-02-05 22:32:13 +0100

    added tesk3

---
+++ javascript/javascript1/week1/houseypricey.js
@@ -0,0 +1,25 @@
+
+let garden1SizeInM2 = 100
+let volume1InMeters = 8 * 10 *10
+let house1Price = volume1InMeters * 2.5 * 1000 + garden1SizeInM2 * 300;
+let house1Cost = 2500000;
+if(house1Cost>house1Price){
+    console.log("Peter is paying too much ")
+}else{
+    console.log("Peter is paying too little")
+}
+
+
+console.log(house1Price);
+
+
+let volume2InMeters = 5 * 11 * 8 
+let garden2SizeInM2 = 70
+let house2Cost = 1000000;
+let house2Price = volume2InMeters * 2.5 * 1000 + garden2SizeInM2 * 300;
+if(house2Cost>house2Price){
+    console.log("Julia is paying too much ")
+}else{
+    console.log("Julia is paying too little")
+}
+console.log(house2Price);
